1c380758116cad6366df6fd23c070e5196d9b698
SopRop <sophie.ropert@wisolv.com>
2020-01-20 18:29:05 +0100

    untracked

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Import all modules
+import { LandingModule } from './landing/landing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LandingModule
+  ]
+})
+export class PagesModule { }
